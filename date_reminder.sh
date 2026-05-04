01ca09ad8f0ca996d6092787650ed5e09673dae9
Kishan Patel <kishanpatel789@gmail.com>
2026-05-04 16:28:45 -0500

    Generate message for date reminder

--- date_reminder.sh
+++ date_reminder.sh
@@ -12,9 +12,12 @@ fi
 TODAY=$(date +%Y-%m-%d)
 TODAY_MD=${TODAY:5}
 
+
 echo "------ $TODAY ------"
 
-while IFS=',' read d_date type person_id display_name; do
+output=""
+
+while IFS="," read d_date type person_id display_name; do
   # skip header
   if [[ $d_date == "date" ]]; then
     continue
@@ -22,6 +25,21 @@ while IFS=',' read d_date type person_id display_name; do
 
 
   #echo "$person_id has $type on $d_date"
-  echo ${d_date:5} is $TODAY_MD?
+  if [[ ${d_date:5} == ${TODAY:5} ]]; then
+    num_years=$[ $(date +%Y) - ${d_date:0:4} ]
+
+    if [[ -n $display_name ]]; then 
+      name=${display_name//\"/}
+    else
+      temp=${person_id%%.*}
+      name=${temp/+/ and }
+    fi
+
+    output+="$person_id - ${type^^} - $num_years\n"
+    output+="Happy $type, $name! 🎉\n"
+    output+="Have a great day! 😀\n\n"
+  fi
 
 done < "$DATA_FILE"
+
+echo -e $output
