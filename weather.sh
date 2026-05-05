43cdb8f013f99808c47fa74e3414d85662584982
Kishan Patel <kishanpatel789@gmail.com>
2026-05-05 16:19:34 -0500

    Add cloud percentage

--- weather.sh
+++ weather.sh
@@ -4,5 +4,6 @@ cat current_weather.json | jq '.dt' | xargs -I{} date -d @{}
 
 cat current_weather.json | jq -r '"\(.weather[0].main) - \(.weather[0].description)"'
 cat current_weather.json | jq -r '"\(.main.temp) Celcius"'
+cat current_weather.json | jq -r '"\(.clouds.all)% cloudy"'
 
-
+# check if rain.1h and snow.1h are present as keys; if so, print
